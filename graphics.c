4aeead2679cf0d8d19847b611a4ce86bd9a7de77
Lauri Gustafsson <luutifa@gmail.com>
2015-09-19 03:11:00 +0300

    Fixed line drawing. Still very slow, fixed point math is next.

--- graphics.c
+++ graphics.c
@@ -3,17 +3,30 @@
 
 void drawLine(vec2 a, vec2 b, vbyte c) {
     float slope = ((float)(b.y - a.y)) / ((float)(b.x - a.x));
-    int x, x1, x2, y1;
+    float slope2 = ((float)(b.x - a.x)) / ((float)(b.y - a.y));
+    int x, y, x1, x2, y1, y2, yx1, xy1;
     if (a.x<b.x) {
         x1=a.x;
         x2=b.x;
-        y1=a.y;
+        yx1=a.y;
     } else {
         x1=b.x;
         x2=a.x;
+        yx1=b.y;
+    }
+    if (a.y<b.y) {
+        y1=a.y;
+        y2=b.y;
+        xy1=a.x;
+    } else {
         y1=b.y;
+        y2=a.y;
+        xy1=b.x;
     }
     for (x=x1; x<x2; x++) {
-        setPixel(x, slope*(x-x1)+y1, 4);
+        setPixel(x, slope*(x-x1)+yx1, c);
+    }
+    for (y=y1; y<y2; y++) {
+        setPixel(slope2*(y-y1)+xy1, y, c);
     }
 }
--- main.c
+++ main.c
@@ -6,14 +6,30 @@
 #include "vectors.h"
 
 int main(int argc, char* argv[]) {
-    vuint x=0;
-    vuint y=0;
-    vec2 a = {20, 20};
-    vec2 b = {80, 80};
+    vuint t=0;
+    vec2 a, a2;
+    vec2 b, b2;
     
     initVideo();
     
-    setPixel(20, 20, 15);
+    for (;;) {
+        a.x=(int)(sin(((float)t)*0.01f)*60.0f+120.0f);
+        b.x=(int)(sin(((float)t)*0.01f+3.1f)*60.0f+120.0f);
+        a.y=(int)(cos(((float)t)*0.01f)*60.0f+120.0f);
+        b.y=(int)(cos(((float)t)*0.01f+3.1f)*60.0f+120.0f);
+        drawLine(a, b, 2);
+        
+        a2.x=(int)(sin(((float)t)*0.01f)*20.0f+80.0f);
+        b2.x=(int)(sin(((float)t)*0.01f+3.1f)*20.0f+80.0f);
+        a2.y=(int)(cos(((float)t)*0.01f)*20.0f+80.0f);
+        b2.y=(int)(cos(((float)t)*0.01f+3.1f)*20.0f+80.0f);
+        drawLine(a2, b2, 2);
+        
+        blitBuffer();
+        clearBuffer();
+        t++;
+    }
+    
     blitBuffer();
     sleep(5);
     
